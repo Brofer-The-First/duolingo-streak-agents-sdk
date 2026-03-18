c765fe8d166d3c709ea31254176e056dcabcae59
Brofer-The-First <ofer.brodatch@gmail.com>
2026-03-18 19:40:56 +0200

    formatting

--- main.py
+++ main.py
@@ -1,22 +1,23 @@
 import asyncio
 import tools
-from agents import Agent, Runner, trace
+from agents import Agent, Runner, RunConfig, trace
 from dotenv import load_dotenv
 
 load_dotenv()  # Load environment variables from .env file
 
-agent = Agent(
+login_agent = Agent(
     name="Login Agent",
-    instructions="You are a browser automation agent. Go to Duolingo.com, click the login button, " \
-                "enter the email and password, and log in to your account." \
-                "Then, start a lesson." \
-                "Action plan:" \
-                "1. take a screenshot" \
-                "2. plan your clicks\types based on the screenshot" \
-                "3. execute your plan by clicking and typing" \
-                "4. take another screenshot to make sure your plan worked. Unless logged in, Repeat steps 2-4." \
-                "5. If browser still loading, wait 5 seconds and take another screenshot. Do this up to 3 times before reporting an error." \
-                "",
+    instructions="""
+        You are a browser automation agent. 
+        Go to Duolingo.com, click the login button, enter the email and password, and log in to your account. Then, start a lesson.
+
+        Action plan:
+        1. take a screenshot
+        2. plan your clicks/types based on the screenshot
+        3. execute your plan by clicking and typing
+        4. take another screenshot to make sure your plan worked. Unless logged in, Repeat steps 2-4 based on the current screenshot.
+        5. If browser still loading, wait 5 seconds and take another screenshot. Do this up to 3 times before reporting an error.
+    """,
     tools=[
         tools.open_duolingo,
         tools.take_screenshot,
@@ -28,13 +29,45 @@ agent = Agent(
     ],
 )
 
+solve_agent = Agent(
+    name="Solve Agent",
+    instructions="""
+        You are a browser automation agent and a native English and French Speaker.
+        You have an open browser with a Duolingo exercise you need to solve.
+        After solving the exercise, continue to the next one and stop there.
+        If it's a listening/speaking exercise, skip it.
+
+        Action plan:
+        1. take a screenshot
+        2. plan your clicks/types based on the screenshot
+        3. execute your plan by clicking and typing
+        4. take another screenshot to make sure your plan worked. If exercise not solved, Repeat steps 2-4 based on the current screenshot.
+    """,
+    tools=[
+        tools.take_screenshot,
+        tools.click,
+        tools.type_text,
+        tools.press_key
+    ],
+)
+
+
 async def main():
     with trace("Duolingo Streak Agent Run"):
-        result = await Runner.run(agent,
-                                  "",
-                                  max_turns=30,
-                                  )
-        print(result.final_output)
+        await Runner.run(
+            login_agent,
+            "",
+            max_turns=30,
+            run_config=RunConfig(model="gpt-4.1"),
+        )
+        while True:
+            await Runner.run(
+                solve_agent,
+                "",
+                max_turns=30,
+                run_config=RunConfig(model="gpt-4.1"),
+            )
+        
 
 if __name__ == "__main__":
     asyncio.run(main())
